d625fa07bcd3bb9044ec02239b7558010a57f66d
Sergio Vela Moreno <sergio.velam@um.es>
2022-01-28 01:51:24 +0100

    Evaluación_nativa

---
+++ data/submit_eval_nativa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p hpc-bio-pacioli
+#SBATCH --chdir=/home/alumno26/lab5-HPC
+#SBATCH -J lab5Submit
+#SBATCH --exclusive
+#SBATCH --output=RESULTADOS.txt
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=sergio.velam@um.es
+
+echo "c++ 13-mers:"
+time ./k-mer13.cpp
+echo #
+echo "c++ 14-mers:"
+time ./k-mer14.cpp
+echo #
+echo "python 13-mers:"
+time python ./k-mer13.py
+echo #
+echo "python 14-mers:"
+time python ./k-mer14.py
